01f69830beaac7a87e4357fdf22e67864d5cc4ea
Salyl Bhagwat <salylsb@gmail.com>
2021-07-09 10:53:39 -0700

    Extract state and country info to help fastering filtering based on location
    Signed-off-by: Salyl Bhagwat <salylsb@gmail.com>

--- gammath_stocks_history.py
+++ gammath_stocks_history.py
@@ -8,6 +8,7 @@ __copyright__ = 'Copyright (c) 2021, Salyl Bhagwat, Gammath Works'
 import yfinance as yf
 from datetime import datetime
 from pathlib import Path
+import sys
 
 #Get summary of past 5 years history
 end_date = datetime.today()
@@ -19,17 +20,21 @@ def get_ticker_info(tsymbol):
     if (len(tsymbol) == 0):
         return None
 
-    ticker = yf.Ticker(tsymbol)
-    stock_history = ticker.history(interval='1d', start=start_date, end=end_date, actions=True,auto_adjust=True)
+    try:
+        ticker = yf.Ticker(tsymbol)
+        stock_history = ticker.history(interval='1d', start=start_date, end=end_date, actions=True,auto_adjust=True)
 
-    print('Stock history dataframe info for ', tsymbol, ':\n')
-    stock_history.info()
+        print('Stock history dataframe info for ', tsymbol, ':\n')
+        stock_history.info()
 
-    path = Tickers_dir / f'{tsymbol}'
-    if not path.exists():
-        path.mkdir(parents=True, exist_ok=True)
+        path = Tickers_dir / f'{tsymbol}'
+        if not path.exists():
+            path.mkdir(parents=True, exist_ok=True)
 
-    #Save the history for reference and processing
-    stock_history.dropna().to_csv(path / f'{tsymbol}_history.csv')
+        #Save the history for reference and processing
+        stock_history.dropna().to_csv(path / f'{tsymbol}_history.csv')
+    except:
+        print('\nError getting stock history ', tsymbol, ': ', sys.exc_info()[0])
+        return None
 
     return path, ticker
--- gammath_stocks_summary.py
+++ gammath_stocks_summary.py
@@ -29,6 +29,8 @@ def get_ticker_summary(tsymbol):
     #pegRatio
     #heldPercentInstitutions
     #heldPercentInsiders
+    #state
+    #country
 
     try:
         trailingPE = stock_summary['trailingPE']
@@ -41,59 +43,73 @@ def get_ticker_summary(tsymbol):
         forwardPE = stock_summary['forwardPE']
     except:
         forwardPE = 0
-        print('\forwardPE not found for ', tsymbol)
-        print('\nError while getting stock info for ', tsymbol, ': ', sys.exc_info()[0])
+        print('\nforwardPE not found for ', tsymbol)
+        print('\nError while getting forwardPE info for ', tsymbol, ': ', sys.exc_info()[0])
 
     try:
         fiftyTwoWeekHigh = stock_summary['fiftyTwoWeekHigh']
     except:
         fiftyTwoWeekHigh = 0
-        print('\fiftyTwoWeekHigh not found for ', tsymbol)
-        print('\nError while getting stock info for ', tsymbol, ': ', sys.exc_info()[0])
+        print('\nfiftyTwoWeekHigh not found for ', tsymbol)
+        print('\nError while getting fiftyTwoWeekHigh info for ', tsymbol, ': ', sys.exc_info()[0])
 
     try:
         fiftyTwoWeekLow = stock_summary['fiftyTwoWeekLow']
     except:
         fiftyTwoWeekLow = 0
-        print('\fiftyTwoWeekLow not found for ', tsymbol)
-        print('\nError while getting stock info for ', tsymbol, ': ', sys.exc_info()[0])
+        print('\nfiftyTwoWeekLow not found for ', tsymbol)
+        print('\nError while getting fiftyTwoWeekLow info for ', tsymbol, ': ', sys.exc_info()[0])
 
     try:
         fiftyDayAverage = stock_summary['fiftyDayAverage']
     except:
         fiftyDayAverage = 0
-        print('\fiftyDayAverage not found for ', tsymbol)
-        print('\nError while getting stock info for ', tsymbol, ': ', sys.exc_info()[0])
+        print('\nfiftyDayAverage not found for ', tsymbol)
+        print('\nError while getting fiftyDayAverage info for ', tsymbol, ': ', sys.exc_info()[0])
 
     try:
         shortRatio = stock_summary['shortRatio']
     except:
         shortRatio = 0
-        print('\shortRatio not found for ', tsymbol)
-        print('\nError while getting stock info for ', tsymbol, ': ', sys.exc_info()[0])
+        print('\nshortRatio not found for ', tsymbol)
+        print('\nError while getting shortRatio info for ', tsymbol, ': ', sys.exc_info()[0])
 
     try:
         pegRatio = stock_summary['pegRatio']
     except:
         pegRatio = 0
-        print('\pegRatio not found for ', tsymbol)
-        print('\nError while getting stock info for ', tsymbol, ': ', sys.exc_info()[0])
+        print('\npegRatio not found for ', tsymbol)
+        print('\nError while getting pegRatio info for ', tsymbol, ': ', sys.exc_info()[0])
 
     try:
         heldPercentInstitutions = stock_summary['heldPercentInstitutions']
     except:
         heldPercentInstitutions = 0
-        print('\heldPercentInstitutions not found for ', tsymbol)
-        print('\nError while getting stock info for ', tsymbol, ': ', sys.exc_info()[0])
+        print('\nheldPercentInstitutions not found for ', tsymbol)
+        print('\nError while getting heldPercentInstitutions info for ', tsymbol, ': ', sys.exc_info()[0])
 
     try:
         heldPercentInsiders = stock_summary['heldPercentInsiders']
     except:
         heldPercentInsiders = 0
-        print('\heldPercentInsiders not found for ', tsymbol)
-        print('\nError while getting stock info for ', tsymbol, ': ', sys.exc_info()[0])
+        print('\nheldPercentInsiders not found for ', tsymbol)
+        print('\nError while getting heldPercentInsiders info for ', tsymbol, ': ', sys.exc_info()[0])
 
-    df = pd.DataFrame({'trailingPE': trailingPE, 'forwardPE': forwardPE, 'fiftyTwoWeekHigh': fiftyTwoWeekHigh, 'fiftyTwoWeekLow': fiftyTwoWeekLow, 'fiftyDayAverage': fiftyDayAverage, 'shortRatio': shortRatio, 'pegRatio': pegRatio, 'heldPercentInstitutions': heldPercentInstitutions, 'heldPercentInsiders': heldPercentInsiders}, index=range(1))
+    try:
+        state = stock_summary['state']
+    except:
+        state = ''
+        print('\ncountry not found for ', tsymbol)
+        print('\nError while getting state info for ', tsymbol, ': ', sys.exc_info()[0])
+
+    try:
+        country = stock_summary['country']
+    except:
+        country = ''
+        print('\ncountry not found for ', tsymbol)
+        print('\nError while getting country info for ', tsymbol, ': ', sys.exc_info()[0])
+
+    df = pd.DataFrame({'trailingPE': trailingPE, 'forwardPE': forwardPE, 'fiftyTwoWeekHigh': fiftyTwoWeekHigh, 'fiftyTwoWeekLow': fiftyTwoWeekLow, 'fiftyDayAverage': fiftyDayAverage, 'shortRatio': shortRatio, 'pegRatio': pegRatio, 'heldPercentInstitutions': heldPercentInstitutions, 'heldPercentInsiders': heldPercentInsiders, 'state': state, 'country': country}, index=range(1))
 
 
     path = Tickers_dir / f'{tsymbol}'
