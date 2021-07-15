589699cce6878849501bc869aa799e3cb3704c19
Salyl Bhagwat <salylsb@gmail.com>
2021-07-15 10:31:19 -0700

    Add beta to the parameters used for buy/sell score computation
    Signed-off-by: Salyl Bhagwat <salylsb@gmail.com>

---
+++ gammath_beta_signals.py
@@ -0,0 +1,35 @@
+# Author: Salyl Bhagwat, Gammath Works
+# Copyright (c) 2021, Salyl Bhagwat, Gammath Works
+# All Rights Reserved
+#
+__author__ = 'Salyl Bhagwat'
+__copyright__ = 'Copyright (c) 2021, Salyl Bhagwat, Gammath Works'
+
+from pathlib import Path
+import pandas as pd
+
+def get_beta_signals(tsymbol, df_summ):
+
+    print('\nGetting beta signals')
+    beta_buy_score = 0
+    beta_sell_score = 0
+    beta_max_score = 0
+
+    beta = df_summ['beta'][0]
+
+    print('Beta for ', tsymbol, ': ', beta)
+
+    if (beta > 0):
+        if (beta < 3):
+            beta_buy_score += 1
+        else:
+            beta_sell_score += 1
+
+    beta_max_score += 1
+
+    beta_buy_rec = f'beta_buy_score:{beta_buy_score}/{beta_max_score}'
+    beta_sell_rec = f'beta_sell_score:{beta_sell_score}/{beta_max_score}'
+
+    beta_signals = f'BETA:{beta},{beta_buy_rec},{beta_sell_rec}'
+
+    return beta_buy_score, beta_sell_score, beta_max_score, beta_signals
