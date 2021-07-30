2d6e08fbb92f98c1d5b0b9ecb2204dfbb427d039
Salyl Bhagwat <salylsb@gmail.com>
2021-07-29 21:46:12 -0700

    Catch an exception if URL opening runs into issues
    Signed-off-by: Salyl Bhagwat <salylsb@gmail.com>

--- gammath_stcktwts.py
+++ gammath_stcktwts.py
@@ -9,6 +9,7 @@ from pathlib import Path
 import time
 import re
 import urllib.request
+import sys
 
 STOCKTWITS_TICKER_ADDR = 'https://stocktwits.com'
 
@@ -31,50 +32,54 @@ def get_stocktwits_ticker_info(tsymbol, path):
     if not path.exists():
         path.mkdir(parents=True, exist_ok=True)
 
-    with urllib.request.urlopen(url) as response:
-        page = response.read()
-        html_page = f'{page}'
-        #Save the page for reference
-        f = open(path / f'{tsymbol}_st_page.html', 'w')
-        f.write(html_page)
-        f.close()
+    st_buy_score = 0
+    st_sell_score = 0
+    st_max_score = 0
 
-        matched_string = pattern_for_sentiment_change.search(html_page)
-        if (matched_string):
-            kw, val = matched_string.groups()
-            print('\nSentiment change data: ', kw, 'Val: ', val)
-            sentiment_change = val
-        else:
-            print('\nSentiment change data NOT found for ticker ', tsymbol)
+    try:
+        with urllib.request.urlopen(url) as response:
+            page = response.read()
+            html_page = f'{page}'
+            #Save the page for reference
+            f = open(path / f'{tsymbol}_st_page.html', 'w')
+            f.write(html_page)
+            f.close()
 
-        matched_string = pattern_for_volume_change.search(html_page)
-        if (matched_string):
-            kw, val = matched_string.groups()
-            print('\Volume change data: ', kw, 'Val: ', val)
-            volume_change = val
-        else:
-            print('\nVolume change data NOT found for ticker ', tsymbol)
+            matched_string = pattern_for_sentiment_change.search(html_page)
+            if (matched_string):
+                kw, val = matched_string.groups()
+                print('\nSentiment change data: ', kw, 'Val: ', val)
+                sentiment_change = val
+            else:
+                print('\nSentiment change data NOT found for ticker ', tsymbol)
 
-        sts_change = 0
-        stv_change = 0
-        st_buy_score = 0
-        st_sell_score = 0
-        st_max_score = 0
+            matched_string = pattern_for_volume_change.search(html_page)
+            if (matched_string):
+                kw, val = matched_string.groups()
+                print('\Volume change data: ', kw, 'Val: ', val)
+                volume_change = val
+            else:
+                print('\nVolume change data NOT found for ticker ', tsymbol)
 
-        if (sentiment_change is not None):
-            sts_change = float(sentiment_change)
-            st_tw_sentiment_change = f'sentiment_change: {sentiment_change}'
+            sts_change = 0
+            stv_change = 0
 
-        if (volume_change is not None):
-            stv_change = float(volume_change)
-            st_tw_volume_change = f'volume_change: {volume_change}'
+            if (sentiment_change is not None):
+                sts_change = float(sentiment_change)
+                st_tw_sentiment_change = f'sentiment_change: {sentiment_change}'
 
-        if ((sts_change > 5.0) and (stv_change > 0)):
-            st_buy_score += 1
-        elif (sts_change < 0):
-            st_sell_score += 1
+            if (volume_change is not None):
+                stv_change = float(volume_change)
+                st_tw_volume_change = f'volume_change: {volume_change}'
 
-        st_max_score += 1
+            if ((sts_change > 5.0) and (stv_change > 0)):
+                st_buy_score += 1
+            elif (sts_change < 0):
+                st_sell_score += 1
+
+            st_max_score += 1
+    except:
+        print('\nError while getting stocktwits html page for ', tsymbol, ': ', sys.exc_info()[0])
 
     st_buy_rec = f'st_sv_buy_score:{st_buy_score}/{st_max_score}'
     st_sell_rec = f'st_sv_sell_score:{st_sell_score}/{st_max_score}'
