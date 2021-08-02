b564a891c94537a030cc4fc55f8e1369d0efb38c
Salyl Bhagwat <salylsb@gmail.com>
2021-08-02 11:44:33 -0700

    Minor optimization to avoid creating ticker instance and fetching data over the internet
    Signed-off-by: Salyl Bhagwat <salylsb@gmail.com>

--- gammath_stocks_financials.py
+++ gammath_stocks_financials.py
@@ -22,13 +22,16 @@ def get_ticker_financials(tsymbol):
     if (len(tsymbol) == 0):
         return None
 
-    ticker = yf.Ticker(tsymbol)
+    qbs_dont_need_fetch = True
+    qcf_dont_need_fetch = True
+    qe_dont_need_fetch = True
+    qf_dont_need_fetch = True
+    reco_dont_need_fetch = True
 
     path = Tickers_dir / f'{tsymbol}'
     if not path.exists():
         path.mkdir(parents=True, exist_ok=True)
 
-    print(f'\nFetching quarterly balance sheet for {tsymbol}')
     try:
         file_exists = (path / f'{tsymbol}_qbs.csv').exists()
 
@@ -39,25 +42,15 @@ def get_ticker_financials(tsymbol):
             dt = time.strftime('%x').split('/')
             if (fct_time[2] == dt[1]):
                 print('No need to get new file')
-                dont_need_fetch = True
+                qbs_dont_need_fetch = True
             else:
                 print('Date mismatch. Need to fetch new file')
-                dont_need_fetch = False
+                qbs_dont_need_fetch = False
         else:
-            dont_need_fetch = False
-
-
-        if not dont_need_fetch:
-            stock_balance_sheet = ticker.quarterly_balancesheet
-            if (len(stock_balance_sheet) > 0):
-                #Save the data for reference and processing
-                stock_balance_sheet.to_csv(path / f'{tsymbol}_qbs.csv')
-            else:
-                print(f'\nBalance sheet for {tsymbol} is empty')
+                qbs_dont_need_fetch = False
     except:
         print(f'\nQuarterly balance sheet for ticker {tsymbol} not found')
 
-    print(f'\nFetching quarterly Cash Flow for {tsymbol}')
     try:
         file_exists = (path / f'{tsymbol}_qcf.csv').exists()
 
@@ -68,25 +61,15 @@ def get_ticker_financials(tsymbol):
             dt = time.strftime('%x').split('/')
             if (fct_time[2] == dt[1]):
                 print('No need to get new file')
-                dont_need_fetch = True
+                qcf_dont_need_fetch = True
             else:
                 print('Date mismatch. Need to fetch new file')
-                dont_need_fetch = False
+                qcf_dont_need_fetch = False
         else:
-            dont_need_fetch = False
-
-
-        if not dont_need_fetch:
-            stock_cash_flow = ticker.quarterly_cashflow
-            if (len(stock_cash_flow) > 0):
-                #Save the data for reference and processing
-                stock_cash_flow.to_csv(path / f'{tsymbol}_qcf.csv')
-            else:
-                print(f'\nCash flow sheet for {tsymbol} is empty')
+            qcf_dont_need_fetch = False
     except:
         print(f'\nQuarterly Cash Flow for ticker {tsymbol} not found')
 
-    print(f'\nFetching quarterly Earnings for {tsymbol}')
     try:
         file_exists = (path / f'{tsymbol}_qe.csv').exists()
 
@@ -97,25 +80,15 @@ def get_ticker_financials(tsymbol):
             dt = time.strftime('%x').split('/')
             if (fct_time[2] == dt[1]):
                 print('No need to get new file')
-                dont_need_fetch = True
+                qe_dont_need_fetch = True
             else:
                 print('Date mismatch. Need to fetch new file')
-                dont_need_fetch = False
+                qe_dont_need_fetch = False
         else:
-            dont_need_fetch = False
-
-
-        if not dont_need_fetch:
-            stock_earnings = ticker.quarterly_earnings
-            #Save the data for reference and processing
-            if (len(stock_earnings) > 0):
-                stock_earnings.to_csv(path / f'{tsymbol}_qe.csv')
-            else:
-                print(f'\nEarnings sheet for {tsymbol} is empty')
+            qe_dont_need_fetch = False
     except:
         print(f'\nQuarterly Earnings for ticker {tsymbol} not found')
 
-    print(f'\nFetching quarterly Financials for {tsymbol}')
     try:
         file_exists = (path / f'{tsymbol}_qf.csv').exists()
 
@@ -126,25 +99,15 @@ def get_ticker_financials(tsymbol):
             dt = time.strftime('%x').split('/')
             if (fct_time[2] == dt[1]):
                 print('No need to get new file')
-                dont_need_fetch = True
+                qf_dont_need_fetch = True
             else:
                 print('Date mismatch. Need to fetch new file')
-                dont_need_fetch = False
+                qf_dont_need_fetch = False
         else:
-            dont_need_fetch = False
-
-
-        if not dont_need_fetch:
-            stock_financials = ticker.quarterly_financials
-            if (len(stock_financials) > 0):
-                #Save the data for reference and processing
-                stock_financials.to_csv(path / f'{tsymbol}_qf.csv')
-            else:
-                print(f'\nFinancials sheet for {tsymbol} is empty')
+            qf_dont_need_fetch = False
     except:
         print(f'\nQuarterly Financials for ticker {tsymbol} not found')
 
-    print(f'\nFetching recommendations for {tsymbol}')
     try:
 
         file_exists = (path / f'{tsymbol}_reco.csv').exists()
@@ -156,24 +119,82 @@ def get_ticker_financials(tsymbol):
             dt = time.strftime('%x').split('/')
             if (fct_time[2] == dt[1]):
                 print('No need to get new file')
-                dont_need_fetch = True
+                reco_dont_need_fetch = True
             else:
                 print('Date mismatch. Need to fetch new file')
-                dont_need_fetch = False
+                reco_dont_need_fetch = False
         else:
-            dont_need_fetch = False
-
-
-        if not dont_need_fetch:
-            stock_recommendations = ticker.recommendations
-            if (len(stock_recommendations) > 0):
-                #Save the data for reference and processing
-                stock_recommendations.to_csv(path / f'{tsymbol}_reco.csv')
-            else:
-                print(f'\nRecommendations sheet for {tsymbol} is empty')
+            reco_dont_need_fetch = False
     except:
         print(f'\nRecommendations for ticker {tsymbol} not found')
 
-    #Play nice
-    time.sleep(random.randrange(MIN_DELAY_BETWEEN_BATCHES, MAX_DELAY_BETWEEN_BATCHES))
+
+    if (qbs_dont_need_fetch and qcf_dont_need_fetch and qe_dont_need_fetch and qf_dont_need_fetch and reco_dont_need_fetch):
+        return
+    else:
+        ticker = yf.Ticker(tsymbol)
+
+        if not qbs_dont_need_fetch:
+            print(f'\nFetching quarterly balance sheet for {tsymbol}')
+            try:
+                stock_balance_sheet = ticker.quarterly_balancesheet
+                if (len(stock_balance_sheet) > 0):
+                    #Save the data for reference and processing
+                    stock_balance_sheet.to_csv(path / f'{tsymbol}_qbs.csv')
+                else:
+                    print(f'\nBalance sheet for {tsymbol} is empty')
+            except:
+                print(f'\nQuarterly balance sheet for ticker {tsymbol} not found')
+
+        if not qcf_dont_need_fetch:
+            print(f'\nFetching quarterly Cash Flow for {tsymbol}')
+            try:
+                stock_cash_flow = ticker.quarterly_cashflow
+                if (len(stock_cash_flow) > 0):
+                    #Save the data for reference and processing
+                    stock_cash_flow.to_csv(path / f'{tsymbol}_qcf.csv')
+                else:
+                    print(f'\nCash flow sheet for {tsymbol} is empty')
+            except:
+                print(f'\nQuarterly Cash flow sheet for ticker {tsymbol} not found')
+
+        if not qe_dont_need_fetch:
+            print(f'\nFetching quarterly Earnings for {tsymbol}')
+            try:
+                stock_earnings = ticker.quarterly_earnings
+                #Save the data for reference and processing
+                if (len(stock_earnings) > 0):
+                    stock_earnings.to_csv(path / f'{tsymbol}_qe.csv')
+                else:
+                    print(f'\nEarnings sheet for {tsymbol} is empty')
+            except:
+                print(f'\nQuarterly Earnings sheet for ticker {tsymbol} not found')
+
+        if not qf_dont_need_fetch:
+            print(f'\nFetching quarterly Financials for {tsymbol}')
+            try:
+                stock_financials = ticker.quarterly_financials
+                if (len(stock_financials) > 0):
+                    #Save the data for reference and processing
+                    stock_financials.to_csv(path / f'{tsymbol}_qf.csv')
+                else:
+                    print(f'\nFinancials sheet for {tsymbol} is empty')
+            except:
+                print(f'\nQuarterly Finances sheet for ticker {tsymbol} not found')
+
+        if not reco_dont_need_fetch:
+            print(f'\nFetching recommendations for {tsymbol}')
+            try:
+                stock_recommendations = ticker.recommendations
+                if (len(stock_recommendations) > 0):
+                    #Save the data for reference and processing
+                    stock_recommendations.to_csv(path / f'{tsymbol}_reco.csv')
+                else:
+                    print(f'\nRecommendations sheet for {tsymbol} is empty')
+            except:
+                print(f'\nRecommendations sheet for ticker {tsymbol} not found')
+
+        #Play nice
+        time.sleep(random.randrange(MIN_DELAY_BETWEEN_BATCHES, MAX_DELAY_BETWEEN_BATCHES))
+
     return
