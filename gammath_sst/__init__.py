73aec166ed1bab1b9730755e17ed7aed4f9c2366
Salyl Bhagwat <salylsb@gmail.com>
2021-10-11 20:06:34 -0700

    Move for packaging structure; Remove obsolete files
    Signed-off-by: Salyl Bhagwat <salylsb@gmail.com>

---
+++ gammath_sst/__init__.py
@@ -0,0 +1,9 @@
+#!/Users/mlalgo/miniconda3/bin/python
+# Author: Salyl Bhagwat, Gammath Works
+# Copyright (c) 2021, Salyl Bhagwat, Gammath Works 
+# All Rights Reserved
+#
+
+__author__= 'Salyl Bhagwat'
+__copyright__ = 'Copyright (c) 2021, Salyl Bhagwat, Gammath Works'
+
