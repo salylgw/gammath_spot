f072797bc246dae87ba135453207da0ec98a3538
Salyl Bhagwat <salylsb@gmail.com>
2021-11-04 16:24:08 -0700

    Fix options date checking bug
    Signed-off-by: Salyl Bhagwat <salylsb@gmail.com>

--- gammath_sst/gammath_stocks_options.py
+++ gammath_sst/gammath_stocks_options.py
@@ -33,7 +33,15 @@ def get_options_data(tsymbol, ticker, path):
             fstat = os.stat(path / f'{tsymbol}_options_dates.csv')
             fct_time = time.ctime(fstat.st_ctime).split(' ')
             dt = time.strftime('%x').split('/')
-            if (fct_time[2] == dt[1]):
+            if (fct_time[2] == ''):
+                fct_date_index = 3
+            else:
+                fct_date_index = 2
+
+            fct_date = int(fct_time[fct_date_index])
+            dt_date = int(dt[1])
+
+            if (fct_date == dt_date):
                 print('No need to get new file')
                 dont_need_fetch = True
             else:
@@ -68,7 +76,16 @@ def get_options_data(tsymbol, ticker, path):
             fstat = os.stat(path / f'{tsymbol}_call_{option_date}.csv')
             fct_time = time.ctime(fstat.st_ctime).split(' ')
             dt = time.strftime('%x').split('/')
-            if (fct_time[2] == dt[1]):
+
+            if (fct_time[2] == ''):
+                fct_date_index = 3
+            else:
+                fct_date_index = 2
+
+            fct_date = int(fct_time[fct_date_index])
+            dt_date = int(dt[1])
+
+            if (fct_date == dt_date):
                 print('No need to get new file')
                 dont_need_fetch = True
             else:
