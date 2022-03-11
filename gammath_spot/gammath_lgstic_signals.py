01c62c6158d6818b4980e32b458fe45ca2da0973
Salyl Bhagwat <salylsb@gmail.com>
2022-03-10 17:05:57 -0800

    Logistic regression: Work-in-progress. Just getting lib APIs to work. Lot needs to be added to this. TBD.
    Signed-off-by: Salyl Bhagwat <salylsb@gmail.com>

---
+++ gammath_spot/gammath_lgstic_signals.py
@@ -0,0 +1,74 @@
+# Author: Salyl Bhagwat, Gammath Works
+# Copyright (c) 2021, Salyl Bhagwat, Gammath Works
+# All Rights Reserved
+#
+__author__ = 'Salyl Bhagwat'
+__copyright__ = 'Copyright (c) 2021-2022, Salyl Bhagwat, Gammath Works'
+
+import pandas as pd
+import numpy as np
+from sklearn.linear_model import LogisticRegression
+
+#This is a WIP. Just getting APIs to work. Lot will change in this file before it gets integrated into scoring
+
+#from scipy.special import expit
+#    1 / (1 + np.exp(-val))
+#    return expit(vals)
+
+#Temp sigmoid for experimentation
+def price_sigmoid(prices):
+
+    prices_len = len(prices)
+
+    #Zero-initialize the sigmoid
+    prices_sigmoid = pd.Series(0, pd.RangeIndex(prices_len))
+
+    #First element of sigmoid is set to 0; next ascending val then 1 else 0
+    for i in range(prices_len-1):
+        if (prices[i] < prices[i+1]):
+            prices_sigmoid[i+1] = 1
+        else:
+            prices_sigmoid[i+1] = 0
+
+
+    return (prices_sigmoid)
+
+#WIP: This is just to Logistic regression API to work. Algorithm will be updated later
+def get_lgstic_signals(tsymbol, df, path):
+    lgstic_signals = ''
+
+    prices_len = len(df.Close)
+    lp = df.Close[prices_len-1]
+    prices = df.Close
+    sigmoid_vals = price_sigmoid(prices)
+
+    y_vals = np.array(sigmoid_vals)
+    x_vals = np.array([x for x in range(prices_len)])
+
+    #Multiple samples for single feature
+    y_vals = y_vals.reshape(-1, 1)
+    x_vals = x_vals.reshape(-1, 1)
+    y_vals = np.ravel(y_vals)
+
+    #Logistic regression model
+    #WIP: experimenting with the API
+    lrm = LogisticRegression(fit_intercept=True, random_state=20, solver='liblinear', max_iter=1000, multi_class='auto').fit(x_vals, y_vals)
+
+    #Get yprediction
+    y_predictions = lrm.predict(x_vals)
+    y_predictions_len = len(y_predictions)
+
+    last_yp = y_predictions[y_predictions_len-1]
+
+    #Get y probability estimates
+    y_proba = lrm.predict_proba(x_vals)
+    y_proba_len = len(y_proba)
+
+    last_yproba = y_proba[y_proba_len-1]
+
+    #Get goodness-of-fit score
+    fit_score = round(lrm.score(x_vals, y_vals), 3)
+
+    lgstic_signals = f'Logistic: lgstic_fit_score:{fit_score},probab_pred:{last_yproba} for labels:{lrm.classes_}'
+
+    return lgstic_signals
