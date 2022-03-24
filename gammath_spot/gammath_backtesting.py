f309b00242ac9e7060d6cba80162277705b45a30
Salyl Bhagwat <salylsb@gmail.com>
2022-03-23 21:57:52 -0700

    WIP: Experimenting with backtesting module for usability
    Signed-off-by: Salyl Bhagwat <salylsb@gmail.com>

---
+++ gammath_spot/gammath_backtesting.py
@@ -0,0 +1,69 @@
+# Author: Salyl Bhagwat, Gammath Works
+# Copyright (c) 2021-2022, Salyl Bhagwat, Gammath Works
+# All Rights Reserved
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+__author__ = 'Salyl Bhagwat'
+__copyright__ = 'Copyright (c) 2021-2022, Salyl Bhagwat, Gammath Works'
+
+# THIS IS WIP: DO NOT USE
+# Experimenting with backtesting framework and getting APIs to work; A lot will change later
+
+import sys
+from pathlib import Path
+import pandas as pd
+import numpy
+from backtesting import Backtest, Strategy
+
+def getgScoresData(data):
+    return numpy.array(data)
+
+class gScoresDataAction(Strategy):
+
+    def init(self):
+        #Just placeholder for now
+        prices = self.I(getgScoresData, self.data.Close)
+
+    def next(self):
+        #Just placeholder for now
+        a = None
+
+#Quick experiment on backtesting module APIs for usability
+def main():
+    try:
+        tsymbol = sys.argv[1]
+    except:
+        print('ERROR: Need ticker symbol as one argument to this Program.')
+        raise ValueError('Missing ticker symbol')
+
+    Tickers_dir = Path('tickers')
+    path = Tickers_dir / f'{tsymbol}'
+
+    try:
+        #Read Stock summary info into DataFrame in format necessary for Backtest
+        df = pd.read_csv(path / f'{tsymbol}_history.csv', index_col='Date', parse_dates=True)
+    except:
+        print('\nERROR: Stock history file not found for symbol ', tsymbol)
+        return
+
+    #Check API params and compatibility
+    backtest = Backtest(df, gScoresDataAction, cash=3000)
+
+    #Check API
+    bt_stats = backtest.run()
+    print(bt_stats)
+
+if __name__ == '__main__':
+    main()
