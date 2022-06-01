ba6385a52b7bfba8c4544bb20850f834e8b5f012
Salyl Bhagwat <salylsb@gmail.com>
2022-06-01 11:06:44 -0700

    Add a basic backtesting example that can be easily understood and can actually be experimented with.
    Signed-off-by: Salyl Bhagwat <salylsb@gmail.com>

--- gammath_spot/gammath_backtesting.py
+++ gammath_spot/gammath_backtesting.py
@@ -27,6 +27,101 @@ import pandas as pd
 import numpy
 from backtesting import Backtest, Strategy
 
+MIN_SH_PREMIUM_LEVEL = -0.375
+MIN_SH_DISCOUNT_LEVEL = 0.375
+
+MIN_TRADING_DAYS_FOR_5_YEARS = 249*5
+
+#Following is a basic example of writing your own strategy for backtesting
+#This is just provided as an example to show one of the ways to do it
+#Remeber, sell criteria is subjective. The method used here could be a way out of a position if one decides to get out; Otherwise, it is not mandatory to sell if one is confident of long-term prospects
+def run_basic_backtest(df, path, tsymbol):
+
+    #Create a data frame to save the stats
+    df_transactions = pd.DataFrame(columns=['Date', 'Buy_Q', 'Sell_Q', 'sh_gScore', 'Price', 'Avg_Price', 'Profit', 'PCT', 'Days_held'], index=range(MIN_TRADING_DAYS_FOR_5_YEARS))
+
+    history_len = len(df)
+
+    previous_close = 0
+    total_shares = 0
+    total_cost = 0
+    avg_price = 0
+    transactions_count = 0
+    days_held = 0
+    profit = 0
+    buy_q = 0
+    marked_for_sell = False
+
+    for i in range(history_len):
+        curr_sh_gscore = df.Total[i]
+        curr_ols_gscore = df.OLS[i]
+        curr_closing_price = df.Close[i]
+
+        if ((curr_sh_gscore >= MIN_SH_DISCOUNT_LEVEL) or (total_shares and (curr_sh_gscore >= 0))):
+
+            marked_for_sell = False
+            if (((curr_closing_price < avg_price) or (not avg_price))): #Check if rising
+                #Mimic a buy
+                buy_q = 1
+
+                #If it is below "expected" average then buy more
+                if (curr_ols_gscore > 0):
+                    buy_q += 1
+
+                #If it fits better and well below "expected" average then buy more
+                if (curr_ols_gscore > 0.5):
+                    buy_q += 1
+
+                #If it fits better and significantly below "expected" average then buy more
+                if (curr_ols_gscore == 1.0):
+                    buy_q += 1
+
+                total_shares += buy_q
+                total_cost += (curr_closing_price*buy_q)
+                df_transactions['Date'][transactions_count] = df.Date[i]
+                df_transactions['Buy_Q'][transactions_count] = buy_q
+                df_transactions['sh_gScore'][transactions_count] = curr_sh_gscore
+                df_transactions['Price'][transactions_count] = round(curr_closing_price, 3)
+                avg_price = total_cost/total_shares
+                df_transactions['Avg_Price'][transactions_count] = round(avg_price, 3)
+                transactions_count += 1
+
+
+        if (((curr_sh_gscore <= MIN_SH_PREMIUM_LEVEL) and total_shares) or marked_for_sell):
+
+            marked_for_sell = True
+            if (curr_closing_price <= df.Close[i-1]): #Check if falling
+                total_cash = (total_shares * curr_closing_price)
+                if (total_cost <= total_cash):
+                    #Mimic sale
+                    profit = total_cash - total_cost
+                    df_transactions['Date'][transactions_count] = df.Date[i]
+                    df_transactions['Sell_Q'][transactions_count] = total_shares
+                    df_transactions['sh_gScore'][transactions_count] = curr_sh_gscore
+                    df_transactions['Price'][transactions_count] = round(curr_closing_price, 3)
+                    df_transactions['Avg_Price'][transactions_count] = round(avg_price, 3)
+                    df_transactions['Profit'][transactions_count] = round(profit, 3)
+                    df_transactions['PCT'][transactions_count] = round(((profit*100)/total_cost), 3)
+                    df_transactions['Days_held'][transactions_count] = days_held
+                    transactions_count += 1
+                    total_shares = 0
+                    total_cost = 0
+                    days_held = 0
+                    avg_price = 0
+                    profit = 0
+                    total_cash = 0
+                    marked_for_sell = 0
+
+        if (total_shares):
+            days_held += 1
+
+    df_transactions = df_transactions.truncate(after=transactions_count-1)
+    df_transactions.to_csv(path / f'{tsymbol}_gtrades_stats.csv')
+
+#Get the main stock history based gScores
+def extractGscores(data):
+    return data.Total
+
 #Implement your own strategy in this class
 class gScoresDataAction(Strategy):
 
@@ -36,28 +131,30 @@ class gScoresDataAction(Strategy):
         self.previous_close = 0
         self.total_shares = 0
         self.total_cost = 0
+        self.sh_gScore = self.I(extractGscores, self.data)
 
     def next(self):
-        #Following is provided to make it easier for you to implement your strategy
-        curr_sh_gscore = self.data.Total[-1]
+        #Following is provided to make it easier for you to implement your own strategy using the gScore and micro-gScores
+        curr_sh_gscore = self.sh_gScore[-1]
         curr_closing_price = self.data.Close[-1]
+
         #You can use multiple micro-gScores from self.data.<micro-gScore>
 
-        # Following is provided as an example of using this framework
-        # Typical way is to check the charts from gscores historian to correlate different params
-        # Then use those correlated fields in your strategy
+        # Following is provided as an example of using this backtesting framework
+        # Typical way is to check the charts from gscores historian to correlate gScore and micro-gScores with price
+        # Then use those correlated fields in your own strategy
         # Then check the backtesting stats on how well your strategy did
+        # If it shows your desired success rate then one can choose to execute on that strategy
 
-        #Following shows how to use the APIs. Actual strategy will need to be implemented
+        # Following shows how to use the APIs. Following is not a strategy.
+        # Actual strategy will need to be implemented
 #        if (self.previous_close):
 #            if (curr_sh_gscore >= self.min_sh_discount_level):
 #                if (curr_closing_price > self.previous_close):
-#                    self.position.close()
 #                    self.buy()
 
 #            if (curr_sh_gscore <= self.min_sh_premium_level):
 #                if (curr_closing_price < self.previous_close):
-#                    self.position.close()
 #                    self.sell()
 
         self.previous_close = curr_closing_price
@@ -70,7 +167,6 @@ class GBT:
 
     def run_backtest(self, tsymbol):
 
-        MIN_TRADING_DAYS_FOR_5_YEARS = 249*5
         try:
             path = self.Tickers_dir / f'{tsymbol}'
 
@@ -88,17 +184,23 @@ class GBT:
             df_gscores_history = pd.read_csv(path / f'{tsymbol}_micro_gscores.csv', index_col='Unnamed: 0')
             #Generate the dataframe that has stock history and gscores history
             df = df.join(df_gscores_history)
-            df.Date = pd.to_datetime(df['Date'])
-            df = df.set_index('Date')
         except:
-            print('\nERROR: Stock history file not found for symbol ', tsymbol)
+            print('\nERROR: Backtesting data initialization failed for symbol ', tsymbol)
             return
 
+
+        #You can call your own backtesting function here. Following is a basic example
+        run_basic_backtest(df, path, tsymbol)
+
+        #Following is done to make it easier to use this with backtesting python framework
+#        df.Date = pd.to_datetime(df['Date'])
+#        df = df.set_index('Date')
+
         #Instantiate Backtest
-        backtest = Backtest(df, gScoresDataAction, cash=20000)
+#        backtest = Backtest(df, gScoresDataAction, cash=20000)
 
         #Back test the strategy using our dataframe
-        bt_stats = backtest.run()
+#        bt_stats = backtest.run()
 
         #Save it for later reference
-        bt_stats.to_csv(path / f'{tsymbol}_bt_stats.csv')
+#        bt_stats.to_csv(path / f'{tsymbol}_bt_stats.csv')
