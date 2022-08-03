f875d807ebaed235d14fed26f58758cc0cc86e27
Salyl Bhagwat <salylsb@gmail.com>
2022-08-03 07:46:35 -0700

    Incorporate probability and 5y price conjecture in backtesting Add short-term and long-term strategy for backtesting
    Signed-off-by: Salyl Bhagwat <salylsb@gmail.com>

--- gammath_spot/gammath_backtesting.py
+++ gammath_spot/gammath_backtesting.py
@@ -19,7 +19,7 @@ __author__ = 'Salyl Bhagwat'
 __copyright__ = 'Copyright (c) 2021-2022, Salyl Bhagwat, Gammath Works'
 
 # THIS IS PROVIDED TO MAKE IT EASY FOR BACKTESTING
-# Users will need to implement their own strategy in the gScoresDataAction class
+# Users will need to implement their own strategy in the gScoresDataAction class or in run_basic_backtest function
 
 import sys
 from pathlib import Path
@@ -36,9 +36,9 @@ MIN_TRADING_DAYS_FOR_5_YEARS = 249*5
 
 #Following is a basic example of writing your own strategy for backtesting
 #This is just provided as an example to show one of the ways to do it
-#Remeber, sell criteria is subjective. The method used here could be a way out of a position if one decides to get out; Otherwise, it is not mandatory to sell if one is confident of long-term prospects
-#It is generally expected that in reality, execution of gScore-based dollar cost averaging would be better than what is used in backtesting since this does not account of news analysis and current information. As described in the guidelines, one would check the news before making buy/sell decisions
-def run_basic_backtest(df, path, tsymbol):
+#Remember, sell criteria is subjective. The methods shown here could be a way out of a position if one decides to get out; It is not meant to be mandatory sell decision. If one is confident of long-term prospects and if the stock does actually do well then perhaps buy and hold might turn out to be the best strategy
+#It is generally expected that in reality, execution of gScore-based dollar cost averaging would be better than what is used in backtesting since this does not account of news analysis and current information that is used in decision-making. As described in the guidelines, one would check the news before making buy/sell decisions
+def run_basic_backtest(df, path, tsymbol, term):
 
     #Create a data frame to save the stats
     df_transactions = pd.DataFrame(columns=['Date', 'Buy_Q', 'Sell_Q', 'sh_gScore', 'Price', 'Avg_Price', 'Profit', 'PCT', 'Days_held', 'Last_Price', 'Stage', 'Notes'], index=range(MIN_TRADING_DAYS_FOR_5_YEARS))
@@ -54,40 +54,75 @@ def run_basic_backtest(df, path, tsymbol):
     buy_q = 0
     marked_for_buy = False
     marked_for_sell = False
+    buy_now = False
+    sell_now = False
     cycle = ''
 
     #Use percentile levels to determine discount, neutral and premium levels
     #This should cover a broad range of stocks and then can be customized and fine tuned for variety of criteria
     MIN_SH_PREMIUM_LEVEL, NEUTRAL_SH_PREMIUM_LEVEL, MIN_SH_DISCOUNT_LEVEL = df.SH_Total.quantile([0.20, 0.5, 0.80])
 
+    #Get the low and high percentiles for next day up/down probability
+    nup_bp, nup_tp = df.NUP.quantile([0.1, 0.9])
+    ndp_tp = round(1 - nup_bp)
+    last_buy_5y_price_ratio = 0
+
     for i in range(2, history_len):
         curr_sh_gscore = df.SH_Total[i]
+        curr_kf_gscore = df.KF[i]
         curr_ols_gscore = df.OLS[i]
-        curr_closing_price = df.Close[i]
+        curr_macd_gscore = df.MACD[i]
+        curr_mfi_gscore = df.MFI[i]
         prev_closing_price = df.Close[i-1]
+        curr_closing_price = df.Close[i]
+        prev_nup = df.NUP[i-1]
+        curr_nup = df.NUP[i]
+        curr_ndp = round(1 - curr_nup, 3)
+        prev_ndp = round(1- prev_nup, 3)
+        curr_tpc5y = df.TPC5Y[i]
 
-        if ((curr_sh_gscore >= MIN_SH_DISCOUNT_LEVEL) or (total_shares and (curr_sh_gscore >= NEUTRAL_SH_PREMIUM_LEVEL))):
+        #Use this ratio to compare
+        curr_5y_price_ratio = (curr_tpc5y/curr_closing_price)
+        last_buy_5y_price_ratio = 0
+
+        #Look for discount zone entry or if it is still in the "buy zone"
+        if (((curr_sh_gscore >= MIN_SH_DISCOUNT_LEVEL) or (total_shares and (curr_sh_gscore >= NEUTRAL_SH_PREMIUM_LEVEL)))):
 
             marked_for_sell = False
             marked_for_buy = True
-            #Basic conservative approach; This should be customized for your own check for bottom
-            if (((curr_closing_price < avg_price) or (not avg_price)) and ((curr_closing_price > prev_closing_price) and (prev_closing_price > df.Close[i-2]))): #Check if lower than our avg buying price and rising [for two concecutive days]
+            buy_now = False
 
-                #Mimic a buy; In reality, buy quantity can also be 0.1 if your broker support dollar based investing and fractional shares buy/sell
-                buy_q = 1
+            #Check if rising for at least a day
+            single_day_rising = (curr_closing_price > prev_closing_price)
 
-                #If it is below "expected" average then buy more
-                if (curr_ols_gscore > 0):
+            # We only want to buy when price is is below our current average price
+            if (total_shares and (curr_closing_price > avg_price)):
+                buy_now = False
+            else:
+                if (term == 'short_term'):
+                    #Check if rising for two concecutive days
+                    consec_two_days_rising = (single_day_rising and (prev_closing_price > df.Close[i-2]))
+                    if (consec_two_days_rising):
+                        # For short-term, we want to buy when we see two consecutive up days
+                        buy_now = True
+                elif (term == 'long_term'):
+                    #For long-term, we want to buy on up day that follows multiple down days (we look at previous day's next day up probability to gauge that)
+                    # If we are buying for long-term, check for reasonable 5Y return conjecture
+                    if ((prev_nup >= nup_tp) and (single_day_rising) and (curr_5y_price_ratio >= 1.5)):
+                        buy_now = True
+                        #Save this for comparison for sell-side check
+                        last_buy_5y_price_ratio = curr_5y_price_ratio
+
+            if (buy_now):
+                buy_q += 1
+
+                if ((curr_ols_gscore >= 0.5) and (curr_kf_gscore >= 0.5) and (curr_macd_gscore >= 0.5) and (curr_mfi_gscore >= 0.3)):
                     buy_q += 1
 
-                #If it fits better and well below "expected" average then buy more
-                if (curr_ols_gscore > 0.5):
+                if ((curr_ols_gscore == 1.0) and (curr_kf_gscore >= 0.9) and (curr_macd_gscore == 1.0) and (curr_mfi_gscore >= 0.7)):
                     buy_q += 1
 
-                #If it fits better and significantly below "expected" average then buy more
-                if (curr_ols_gscore == 1.0):
-                    buy_q += 1
-
+            if (buy_q):
                 total_shares += buy_q
                 total_cost += (curr_closing_price*buy_q)
                 df_transactions['Date'][transactions_count] = df.Date[i]
@@ -97,26 +132,64 @@ def run_basic_backtest(df, path, tsymbol):
                 avg_price = total_cost/total_shares
                 df_transactions['Avg_Price'][transactions_count] = round(avg_price, 3)
                 transactions_count += 1
+                buy_q = 0
         else:
             marked_for_buy = False
+            buy_now = False
 
 
+        #Check for premium level
         if (((curr_sh_gscore <= MIN_SH_PREMIUM_LEVEL) and total_shares) or marked_for_sell):
             marked_for_buy = False
             marked_for_sell = True
-            #Check if falling
-            if ((curr_closing_price < prev_closing_price) and (prev_closing_price < df.Close[i-2])):
-                total_cash = (total_shares * curr_closing_price)
-                if (total_cost < total_cash):
+            sell_now = False
+
+            total_cash = (total_shares * curr_closing_price)
+            profit = total_cash - total_cost
+            profit_pct = round(((profit*100)/total_cost), 3)
+
+            #Check if in profit
+            if (total_cost < total_cash):
+                #Check if price has fallen for two consecutive days
+                consec_two_days_falling = ((curr_closing_price < prev_closing_price) and (prev_closing_price < df.Close[i-2]))
+
+                #Check if falling for two consecutive days
+                if (consec_two_days_falling):
+                    if (term == 'short_term'):
+                        #For short-term: just sell
+                        sell_now = True
+                    elif (term == 'long_term'):
+                        #For long-term, we need more checks
+                        sell_now = False
+
+                        #If we already doubled then just sell irrespective of holding period
+                        if (profit_pct > 100):
+                            sell_now = True
+                        elif ((curr_5y_price_ratio < 2) and (curr_5y_price_ratio < last_buy_5y_price_ratio)):
+                            #If it doesn't seem to be doubling in 5 years and if our buy-time 5y price ratio is higher than current 5y price ratio then sell
+                            sell_now = True
+                        else:
+                            #If it is < 5% then it is perhaps not going very far
+                            if (profit_pct < 5):
+                                sell_now = True
+                            else:
+                                #Check number of years we've been holding
+                                num_years = days_held/249
+                                # we tend to give it at least 3 years to perform
+                                if (num_years >= 3):
+                                    # If it is not showing at least 20% return per year then sell
+                                    if (profit_pct < (num_years*20)):
+                                        sell_now = True
+
+                if (sell_now):
                     #Mimic sale
-                    profit = total_cash - total_cost
                     df_transactions['Date'][transactions_count] = df.Date[i]
                     df_transactions['Sell_Q'][transactions_count] = total_shares
                     df_transactions['sh_gScore'][transactions_count] = curr_sh_gscore
                     df_transactions['Price'][transactions_count] = round(curr_closing_price, 3)
                     df_transactions['Avg_Price'][transactions_count] = round(avg_price, 3)
                     df_transactions['Profit'][transactions_count] = round(profit, 3)
-                    df_transactions['PCT'][transactions_count] = round(((profit*100)/total_cost), 3)
+                    df_transactions['PCT'][transactions_count] = profit_pct
                     df_transactions['Days_held'][transactions_count] = days_held
                     transactions_count += 1
                     total_shares = 0
@@ -126,6 +199,7 @@ def run_basic_backtest(df, path, tsymbol):
                     profit = 0
                     total_cash = 0
                     marked_for_sell = 0
+                    sell_now = False
         else:
             marked_for_sell = False
 
@@ -149,10 +223,14 @@ def run_basic_backtest(df, path, tsymbol):
     else:
         cycle = 'Hold_cycle'
 
+    if (total_shares):
+        df_transactions.Days_held[transactions_count] = days_held
+        df_transactions.PCT[transactions_count] = profit_pct
+
     df_transactions.Stage[transactions_count] = cycle
     df_transactions.Notes[transactions_count] = note
     df_transactions = df_transactions.truncate(after=transactions_count)
-    df_transactions.to_csv(path / f'{tsymbol}_gtrades_stats.csv')
+    df_transactions.to_csv(path / f'{tsymbol}_gtrades_stats_{term}.csv')
 
 #Get the main stock history based gScores
 def extractGscores(data):
@@ -231,7 +309,8 @@ class GBT:
 
 
         #You can call your own backtesting function here. Following is a basic example
-        run_basic_backtest(df, path, tsymbol)
+        run_basic_backtest(df, path, tsymbol, 'short_term')
+        run_basic_backtest(df, path, tsymbol, 'long_term')
 
         #Following is done to make it easier to use this with backtesting python framework
 #        df.Date = pd.to_datetime(df['Date'])
