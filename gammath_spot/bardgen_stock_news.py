09a862e1e19d0d5274d959987e6d1859ddc3c546
Salyl Bhagwat <salylsb@gmail.com>
2023-05-09 15:42:07 -0700

    WIP: Stock news scraping. This is experimental code using Bard and is barely tested.
    Signed-off-by: Salyl Bhagwat <salylsb@gmail.com>

---
+++ gammath_spot/bardgen_stock_news.py
@@ -0,0 +1,63 @@
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+#The source code in file is generated using Bard
+#I've (Salyl Bhagwat) only made minor modifications (for compatibility and to make it work with Gammath SPOT)
+#Please note that is experimental, barely tested and work-in-progress
+
+import requests
+from bs4 import BeautifulSoup
+
+def get_stock_news(tsymbol, start_date, end_date):
+
+    # Make a request to the Google News API
+    url = "https://news.google.com/news/feeds?q={}&output=rss&start_date={}&end_date={}".format(tsymbol, start_date, end_date)
+
+    response = requests.get(url)
+
+    # Parse the RSS feed
+    soup = BeautifulSoup(response.content, "html.parser")
+
+    # Extract the news articles
+    articles = soup.find_all("item")
+
+    # Create a list to store the news articles
+    news_articles = []
+
+    # Iterate over the news articles
+    for article in articles:
+
+        # Get the title
+        title = article.find("title").text
+
+        # Get the description
+        description = article.find("description").text
+
+        # Get the date
+        date = article.find("pubdate").text
+
+        # Get the link
+        link = article.find("link").text
+
+        # Create a dictionary to store the news article
+        news_article = {
+            "title": title,
+            "description": description,
+            "date": date,
+            "link": link
+        }
+
+        # Add the news article to the list
+        news_articles.append(news_article)
+
+    return news_articles
