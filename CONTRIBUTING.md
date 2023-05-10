a945af5762316a7c9533fa0e333a03af3929e71a
Salyl Bhagwat <salylsb@gmail.com>
2023-05-10 10:32:57 -0700

    WIP: Add Stochastic indicator alternative (based on ChatGPT generated code)
    Signed-off-by: Salyl Bhagwat <salylsb@gmail.com>

--- CONTRIBUTING.md
+++ CONTRIBUTING.md
@@ -3,13 +3,15 @@
 It is April 2023 and all the features necessary for SPOT are now complete. I have added documentation to the point where it can be opened up for collaboration. The intent to make SPOT (**S**tock **P**rice-**O**pining **T**ool) an open source project is to make it transparent, to invite scrutiny of the source code, and to collaborate for making it one of the best and comprehensive stock analysis toolset that can benefit investors worldwide. If you have Python, Finance/Investing, and/or Machine learning expertise and would like to participate in this project then please let me know (You can use "contact" form on [https://www.gammathworks.com](URL) or simply send me an email at salylgw@gmail.com).
 
 
+**Feature In Progress**
+
+  * News sentiment analysis and scoring.
+
 **TBD features for collaboration**
 
   * Explore and include use of other data sources (regional and international) for the data scraper tool. I have seen that yfinance works for some stocks listed on exchanges outside the US but I haven't tested those much. [sample_watchlist.csv](https://github.com/salylgw/gammath_spot/blob/main/gammath_spot/sample_watchlist.csv) includes examples of stocks listed outside the US and needs to be expanded.
   * For stocks listed outside the US, related international/regional social networking sites (e.g. stocktwits) need to be explored and used in the toolset
-  * News sentiment analysis and scoring (using tools such as Natural Language Tool Kit, Spacy, Textblob etc.) needs to be implemented. I see that yfinance supports getting the news so that can be used a news source.
-  * Fundamental analysis enhancements
-  * Any improvements (new features, bug fixes [if any], documentation, optimizations etc.).
+  * Any improvements (new features, improvements, bug fixes [if any], documentation, optimizations etc.).
 
 
 **HOWTO get involved**
--- gammath_spot/cgptgen_tilib.py
+++ gammath_spot/cgptgen_tilib.py
@@ -12,9 +12,12 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 #The source code in file is generated using ChatGPT
-#I've (Salyl Bhagwat) only made minor modifications for compatibility
+#I've (Salyl Bhagwat) only made minor modifications for compatibility.
 
 #Please note that is experimental and barely tested
+#The reason for this file. Many users have expressed problem installing talib.
+#Installing from conda-forge channel resolves it but there have been some users who don't prefer that way.
+#There are other options but now that I have help from ChatGPT....
 
 import pandas as pd
 import numpy as np
@@ -52,6 +55,7 @@ def compute_rsi(prices, window=14):
         rs = up / down
         rsi[i] = 100. - 100. / (1. + rs)
 
+    #Return in pandas series
     rsi = pd.Series(rsi)
 
     return rsi
@@ -90,7 +94,9 @@ def compute_mfi(high_prices, low_prices, close_prices, volumes, period=14):
 
         mfi_values.append(mfi)
 
+    #Return in pandas series
     mfi = pd.Series(mfi_values)
+
     return mfi
 
 def compute_macd(prices, slow_period=26, fast_period=12, signal_period=9):
@@ -104,6 +110,7 @@ def compute_macd(prices, slow_period=26, fast_period=12, signal_period=9):
     macd_line = pd.Series(macd_line)
     signal_line = pd.Series(signal_line)
     histogram = pd.Series(histogram)
+
     return macd_line, signal_line, histogram
 
 def calculate_ema(prices, period):
@@ -115,3 +122,44 @@ def calculate_ema(prices, period):
         ema[i] = (prices[i] - ema[i-1]) * smoothing_factor + ema[i-1]
 
     return ema
+
+#I prefer talib's STOCH function. This implementation from ChatGPT could be a good starting point as an alternative
+def compute_stoch(close_prices, low_prices, high_prices, k_period, d_period, slowing_period):
+
+    highest_high = np.max(high_prices)
+    lowest_low = np.min(low_prices)
+
+    # Calculate %K line
+    k_line = (close_prices - lowest_low) / (highest_high - lowest_low) * 100
+    k_line = simple_moving_average(k_line, k_period)
+
+    # Calculate %D line
+    d_line = simple_moving_average(k_line, d_period)
+
+    # Calculate slow %K line
+    slow_k_line = simple_moving_average(k_line, slowing_period)
+
+    # Calculate slow %D line using exponential moving average
+    slow_d_line = exponential_moving_average(slow_k_line, slowing_period)
+
+    #Return in pandas series
+    slow_k_line = pd.Series(slow_k_line)
+    slow_d_line = pd.Series(slow_d_line)
+
+    return slow_k_line, slow_d_line
+
+def simple_moving_average(values, n):
+
+    sma = np.convolve(values, np.ones(n)/n, mode='valid')
+    nan_padding = np.full(n-1, np.nan)
+
+    return np.concatenate((nan_padding, sma), axis=0)
+
+def exponential_moving_average(values, n):
+
+    weights = np.exp(np.linspace(-1., 0., n))
+    weights /= weights.sum()
+    ema = np.convolve(values, weights, mode='valid')
+    nan_padding = np.full(n-1, np.nan)
+
+    return np.concatenate((nan_padding, ema), axis=0)
