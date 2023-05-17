53ac5ab0da517c5d8aa6d92a17d5e2a760f29bf2
Salyl Bhagwat <salylsb@gmail.com>
2023-05-17 11:20:00 -0700

    Update docs Release v10.8
    Signed-off-by: Salyl Bhagwat <salylsb@gmail.com>

--- gammath_spot/gammath_utils.py
+++ gammath_spot/gammath_utils.py
@@ -38,7 +38,7 @@ MIN_TRADING_DAYS_PER_YEAR = 239
 MIN_TRADING_DAYS_FOR_5_YEARS = (MIN_TRADING_DAYS_PER_YEAR*5)
 
 def get_gammath_spot_version_string():
-    return 'version 10.7'
+    return 'version 10.8'
 
 def set_child_process_start_method():
 
