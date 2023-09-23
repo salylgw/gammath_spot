f1cc46f4b1e67ae5dd51bf40bc91a12de95b4c23
Salyl Bhagwat <salylsb@gmail.com>
2023-09-23 15:56:19 -0700

    v11.0.1 (with yfinance version set)
    Signed-off-by: Salyl Bhagwat <salylsb@gmail.com>

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ def readme():
         return f.read()
 
 setup(name='gammath_spot',
-      version='11',
+      version='11.0.1',
       description='Stock Price-Opining Tools',
       long_description=readme(),
       long_description_content_type='text/markdown',
@@ -33,7 +33,7 @@ setup(name='gammath_spot',
       author='Salyl Bhagwat',
       author_email='salylgw@gmail.com',
       packages=['gammath_spot'],
-      install_requires=['numpy', 'pandas', 'pandas_datareader', 'ta-lib', 'yfinance', 'pykalman', 'statsmodels', 'matplotlib', 'scikit-learn', 'backtesting', 'textblob',],
+      install_requires=['numpy', 'pandas', 'pandas_datareader', 'ta-lib', 'yfinance==0.2.28', 'pykalman', 'statsmodels', 'matplotlib', 'scikit-learn', 'backtesting', 'textblob',],
       include_package_data=True,
       package_data={'gammath_spot': ['data/logo.png']},
       entry_points = {
