166286b64ef02f712686f18dc1b5337b80a48163
Salyl Bhagwat <salylgw@gmail.com>
2023-10-26 16:55:05 -0700

    Work-In-Progress: Init model tuning params, reorg code into class.
    Signed-off-by: Salyl Bhagwat <salylgw@gmail.com>

--- gammath_spot/gammath_rnn_predictor.py
+++ gammath_spot/gammath_rnn_predictor.py
@@ -30,6 +30,7 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import LSTM, Dense
 from tensorflow.keras.callbacks import EarlyStopping
 from tensorflow.keras import optimizers
+import keras_tuner as tuner
 
 try:
     from gammath_spot import gammath_utils as gut
@@ -37,89 +38,100 @@ except:
     import gammath_utils as gut
 
 #This is experimental, Work-In-Progress and barely tested. Lot will change before it is usable
-#The purpose of this function is to generate data for lookahead number of timesteps
-#Generally, more time steps into the future compounds the errors so it is preferred
-#to use smaller number of lookahead timesteps. Ideal lookahead_time_steps=1.
-#Predictions are likely to reflect a trend (increasing or decreasing).
-#This function can be called again with new augmented data to retrain and do further lookahead
-def do_rnn_lstm_lookahead(data, lookahead_time_steps, need_scaling):
-    #re-arrange data in 63-time steps and output
-    ar_size = 63
+class GRNN:
+    def __init__(self):
+        self.ar_size = 63
 
-    #Align data based on AR input size
-    start_offset = (len(data)%ar_size)
-    data = data[start_offset:]
+    def init_model_tuning_params(self):
+        self.hp = tuner.HyperParameters()
+        self.hp_units = self.hp.Int('units', min_value=4, max_value=64, step=10)
+        self.hp_activations = self.hp.Choice('activations', ['linear', 'relu', 'tanh'])
+        self.hp_optimizers = self.hp.Choice('optimizer', ['sgd', 'rmsprop', 'adam'])
+        self.hp_learning_rate = self.hp.Choice('learning_rate', [1e-4, 1e-3, 1e-3])
 
-    #Make it 2D
-    data = np.array(data).reshape(-1, 1)
+    #The purpose of this function is to generate data for lookahead number of timesteps
+    #Generally, more time steps into the future compounds the errors so it is preferred
+    #to use smaller number of lookahead timesteps. Ideal lookahead_time_steps=1.
+    #Predictions are likely to reflect a trend (increasing or decreasing).
+    #This function can be called again with new augmented data to retrain and do further lookahead
+    def do_rnn_lstm_lookahead(self, data, lookahead_time_steps, need_scaling):
+        #re-arrange data in 63-time steps and output
+        self.ar_size = 63
 
-    if (need_scaling):
-        scaler = MinMaxScaler()
-        #Scale data between 0 and 1
-        scaled_data = scaler.fit_transform(data)
-    else:
-        #Already scaled input
-        scaled_data = data
+        #Align data based on AR input size
+        start_offset = (len(data)%self.ar_size)
+        data = data[start_offset:]
 
-    y = scaled_data[ar_size:]
-    n = scaled_data.shape[0]
-    x = np.hstack(tuple([scaled_data[i: n-j, :] for i, j in enumerate(range(ar_size, 0, -1))]))
+        #Make it 2D
+        data = np.array(data).reshape(-1, 1)
 
-    #convert x to 3D for LSTM-RNN
-    x = x.reshape(-1, ar_size, 1)
+        if (need_scaling):
+            scaler = MinMaxScaler()
+            #Scale data between 0 and 1
+            scaled_data = scaler.fit_transform(data)
+        else:
+            #Already scaled input
+            scaled_data = data
 
-    x_len = len(x)
-    y_len = len(y)
+        y = scaled_data[self.ar_size:]
+        n = scaled_data.shape[0]
+        x = np.hstack(tuple([scaled_data[i: n-j, :] for i, j in enumerate(range(self.ar_size, 0, -1))]))
 
-    #Use last one year for test set
-    x_test_offset = (x_len-252)
-    y_test_offset = (y_len-252)
+        #convert x to 3D for LSTM-RNN
+        x = x.reshape(-1, self.ar_size, 1)
 
-    x_train = x[:x_test_offset]
-    y_train = y[:y_test_offset]
-    x_test = x[x_test_offset:]
-    y_test = y[y_test_offset:]
+        x_len = len(x)
+        y_len = len(y)
 
-    #Using GPU is much faster but there are some portability issues to be resolved
-    #Until then, use CPU for this
+        #Use last one year for test set
+        x_test_offset = (x_len-252)
+        y_test_offset = (y_len-252)
 
-    #TBD: Tune hyperparameters with keras_tuner
+        x_train = x[:x_test_offset]
+        y_train = y[:y_test_offset]
+        x_test = x[x_test_offset:]
+        y_test = y[y_test_offset:]
 
-    #Run some experiments for single feature regression
-    if (need_scaling):
-        #Scaled using minmax scaler between 0 and 1
-        model = Sequential([LSTM(units=32, input_shape=(ar_size, 1), name='LSTM', return_sequences=False), Dense(1, name='Output')])
-    else:
-        #Already scaled data (in this case SH_gScore is between -0.75 and +0.75)
-        model = Sequential([LSTM(units=32, activation='tanh', input_shape=(ar_size, 1), name='LSTM', return_sequences=False), Dense(1, activation='tanh', name='Output')])
+        #Using GPU is much faster but there are some portability issues to be resolved
+        #Until then, use CPU for this
 
-    #Compile the model with popular optimizer and MSE for regression
-    model.compile(optimizer='adam', loss='mean_squared_error')
+        #TBD: Tune hyperparameters with keras_tuner
 
-    #Use early stopping
-    early_stopping = EarlyStopping(monitor='val_loss', patience=20, restore_best_weights=True)
+        #Run some experiments for single feature regression
+        if (need_scaling):
+            #Scaled using minmax scaler between 0 and 1
+            model = Sequential([LSTM(units=32, input_shape=(self.ar_size, 1), name='LSTM', return_sequences=False), Dense(1, name='Output')])
+        else:
+            #Already scaled data (in this case SH_gScore is between -0.75 and +0.75)
+            model = Sequential([LSTM(units=32, activation='tanh', input_shape=(self.ar_size, 1), name='LSTM', return_sequences=False), Dense(1, activation='tanh', name='Output')])
 
-    #Train the model with training set
-    lstm_training = model.fit(x_train, y_train, epochs=100, validation_data=(x_test, y_test), callbacks=[early_stopping], verbose=1)
+        #Compile the model with popular optimizer and MSE for regression
+        model.compile(optimizer='adam', loss='mean_squared_error')
 
-    #Get the evaluation results for debugging
-    evaluation = model.evaluate(x_test, y_test)
+        #Use early stopping
+        early_stopping = EarlyStopping(monitor='val_loss', patience=20, restore_best_weights=True)
 
-    predicted_values = []
+        #Train the model with training set
+        lstm_training = model.fit(x_train, y_train, epochs=100, validation_data=(x_test, y_test), callbacks=[early_stopping], verbose=1)
 
-    for i in range(lookahead_time_steps):
-        #Predict (eventually in a loop to predict t+k steps into the future)
-        y_predict = model.predict(x_test)
-        y_predict_len = len(y_predict)
-        predicted_values.append(y_predict[y_predict_len-1][0])
-        #Rearrange current predicted value into x_test and loop
-        new_row = np.concatenate([x_test[x_test.shape[0]-1:, 1:, :], y_predict[None, (y_predict_len-1):, :]], axis=1)
-        x_test = np.concatenate([x_test[1:, :, :], new_row[:, :, :]])
+        #Get the evaluation results for debugging
+        evaluation = model.evaluate(x_test, y_test)
 
-    if (need_scaling):
-        #Unscale the values (need to do this for predicted_values also
-        predicted_values = scaler.inverse_transform(np.array(predicted_values).reshape(-1, 1))
-        predicted_values = predicted_values.flatten()
+        predicted_values = []
 
-    #return values predicted for lookahead_time_steps
-    return predicted_values
+        for i in range(lookahead_time_steps):
+            #Predict (eventually in a loop to predict t+k steps into the future)
+            y_predict = model.predict(x_test)
+            y_predict_len = len(y_predict)
+            predicted_values.append(y_predict[y_predict_len-1][0])
+            #Rearrange current predicted value into x_test and loop
+            new_row = np.concatenate([x_test[x_test.shape[0]-1:, 1:, :], y_predict[None, (y_predict_len-1):, :]], axis=1)
+            x_test = np.concatenate([x_test[1:, :, :], new_row[:, :, :]])
+
+        if (need_scaling):
+            #Unscale the values (need to do this for predicted_values also
+            predicted_values = scaler.inverse_transform(np.array(predicted_values).reshape(-1, 1))
+            predicted_values = predicted_values.flatten()
+
+        #return values predicted for lookahead_time_steps
+        return predicted_values
