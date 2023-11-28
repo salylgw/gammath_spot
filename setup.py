ce4f65a3b0d0ecceb8a337452f2fda3b39203e57
Salyl Bhagwat <salylgw@gmail.com>
2023-11-28 11:24:23 -0800

    v11.82
    Signed-off-by: Salyl Bhagwat <salylgw@gmail.com>

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ def readme():
         return f.read()
 
 setup(name='gammath_spot',
-      version='11.81',
+      version='11.82',
       description='Stock Price-Opining Tools',
       long_description=readme(),
       long_description_content_type='text/markdown',
